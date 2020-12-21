a6080c51b01f9f6536bdb3ef852654487e209393
Kavya Ravella <kravella@hawk.iit.edu>
2020-12-20 23:21:01 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Kavya Ravella on 12/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
